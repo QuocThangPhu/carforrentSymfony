4f8ca1d5799c14cd2f29e52580d4cf82b4f75ea5
QuocThangPhu <104897205+QuocThangPhu@users.noreply.github.com>
2022-06-09 10:53:16 +0700

    Create README.md
    feat:Setup doctrine and profiler

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class ProductController extends AbstractController
+{
+    #[Route('/product', name: 'app_product')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        $products = $doctrine->getRepository(Product::class)->findAll();
+        return $this->render('product/index.html.twig', [
+            'products' => $products,
+        ]);
+    }
+
+    /**
+     * @Route("/product/create", name="create_product")
+     */
+    public function createProduct(ManagerRegistry $doctrine, ValidatorInterface $validator): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        $product = new Product();
+        $product->setName('banana');
+        $product->setPrice(99);
+        $product->setPicture('https://www.nipponexpress.com/press/report/img/06-Nov-20-ogp.jpeg');
+
+        $errors = $validator->validate($product);
+        if (count($errors) > 0) {
+            return new Response((string) $errors, 400);
+        }
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($product);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return $this->redirectToRoute('product_show', [
+            'id' => $product->getId()
+        ]);
+    }
+
+    /**
+     * @Route("/product/{id}", name="product_show")
+     */
+    public function show(Product $product): Response
+    {
+        // or render a template
+        // in the template, print things with {{ product.name }}
+         return $this->render('product/show.html.twig', ['product' => $product]);
+    }
+
+    /**
+     * @Route("/product/edit/{id}")
+     */
+    public function update(ManagerRegistry $doctrine, int $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $product = $entityManager->getRepository(Product::class)->find($id);
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+        $product->setName('apple');
+        $product->setPicture('apple.jpg');
+        $entityManager->flush();
+
+        return $this->redirectToRoute('product_show', [
+            'id' => $product->getId()
+        ]);
+    }
+
+    /**
+     * @Route("/product/find/{name}")
+     */
+
+    public function findProductByName(ManagerRegistry $doctrine, string $name): Response
+    {
+        $products = $doctrine->getRepository(Product::class)->findByName($name);
+//        return $this->render('product/show.html.twig', ['product' => $product]);
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products,
+        ]);
+    }
+}
