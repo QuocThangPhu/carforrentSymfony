8b6e3defa0667c9add094d64253123f556448283
QuocThang <thang.phu@nfq.asia>
2022-06-13 17:32:47 +0700

    feat: Add Login function and logout

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->isGranted('ROLE_USER')) {
+            return $this->redirectToRoute('profile');
+        }
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUserName = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/index.html.twig', [
+            'controller_name' => 'SecurityController',
+            'lastUserName' => $lastUserName,
+            'error' => $error
+        ]);
+    }
+}
