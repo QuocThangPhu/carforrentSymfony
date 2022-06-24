e42fd3b11fdf011647b610fba6398ccdb42185ca
phuqthang <thang.phu@nfq.asia>
2022-06-24 17:59:27 +0700

    create function send mail with Zoho

---
+++ src/Controller/Api/MailController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Service\MailService;
+use App\Traits\ResponseTrait;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/api/mails', name: 'mail_')]
+class MailController extends AbstractController
+{
+    use ResponseTrait;
+    #[Route('/', name: 'send_mail', methods: 'GET')]
+    public function sendMailToUser(MailService $mailService)
+    {
+        $mailService->sendMail();
+        return $this->success([]);
+    }
+}
--- src/Service/MailService.php
+++ src/Service/MailService.php
@@ -4,38 +4,38 @@ namespace App\Service;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 
 class MailService
 {
+    private ParameterBagInterface $param;
+    public function __construct(ParameterBagInterface $param)
+    {
+        $this->param = $param;
+    }
+
     public function sendMail(): void
     {
         $mail = new PHPMailer(true);
 
         try {
-            $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+            $mail->SMTPDebug = SMTP::DEBUG_OFF;
             $mail->isSMTP();
-            $mail->Host       = 'smtp.example.com';
+            $mail->Host       = $this->param->get('email_host');
+            $mail->CharSet = 'UTF-8';
             $mail->SMTPAuth   = true;
-            $mail->Username   = 'user@example.com';
-            $mail->Password   = 'secret';
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-            $mail->Port       = 465;
+            $mail->Username   = $this->param->get('email_username');
+            $mail->Password   = $this->param->get('email_password');
+            $mail->SMTPSecure = $this->param->get('email_smtp');
+            $mail->Port       = $this->param->get('email_port');
 
-            $mail->setFrom('from@example.com', 'Mailer');
-            $mail->addAddress('joe@example.net', 'Joe User');
-            $mail->addAddress('ellen@example.com');
-            $mail->addReplyTo('info@example.com', 'Information');
-            $mail->addCC('cc@example.com');
-            $mail->addBCC('bcc@example.com');
-
-            $mail->addAttachment('/var/tmp/file.tar.gz');
-            $mail->addAttachment('/tmp/image.jpg', 'new.jpg');
+            $mail->setFrom('support@diggory.me', 'Mailer');
+            $mail->addAddress('dangtran.khoa@nfq.asia', 'Sayno');
 
             $mail->isHTML(true);
-            $mail->Subject = 'Here is the subject';
-            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+            $mail->Subject = 'Khoa ngu';
+            $mail->Body    = 'Khoa nguuuuuuuuuu';
 
             $mail->send();
             echo 'Message has been sent';
