8fcc33f592ecec0d7bdac81f9826d5bc1b8983c1
phuqthang <thang.phu@nfq.asia>
2022-06-24 10:39:06 +0700

    feat:add command

--- src/Command/ProducerCommand.php
+++ src/Command/ProducerCommand.php
@@ -2,18 +2,44 @@
 
 namespace App\Command;
 
+use Aws\Exception\AwsException;
+use Aws\Sqs\SqsClient;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
 
 #[AsCommand(name: 'app:producer')]
 class ProducerCommand extends Command
 {
+    private SqsClient $sqsClient;
+    private ContainerBagInterface $params;
+
+    public function __construct(SqsClient $sqsClient, ContainerBagInterface $params, string $name = null)
+    {
+        parent::__construct($name);
+        $this->sqsClient = $sqsClient;
+        $this->params = $params;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $output->writeln('Hello QuocThang!!');
-        $output->writeln('Whoa! You so beautiful honey');
+        $sqsUrl = $this->params->get('sqsUrl');
+        $params = [
+            'MessageBody' => "producer Diggory created this message!!!",
+            'QueueUrl' => $sqsUrl
+        ];
+        try {
+            $this->sqsClient->sendMessage($params);
+        } catch (AwsException $e) {
+            error_log($e->getMessage());
+        }
+        $output->writeln('Producer send!');
 
         return Command::SUCCESS;
     }
--- src/Command/WorkerCommand.php
+++ src/Command/WorkerCommand.php
@@ -2,18 +2,57 @@
 
 namespace App\Command;
 
+use Aws\Exception\AwsException;
+use Aws\Sqs\SqsClient;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
 
 #[AsCommand(name: 'app:worker')]
 class WorkerCommand extends Command
 {
+    protected static $defaultName = 'app:worker';
+
+    private SqsClient $sqsClient;
+    private ContainerBagInterface $params;
+
+    public function __construct(SqsClient $sqsClient, ContainerBagInterface $params, string $name = null)
+    {
+        parent::__construct();
+        $this->sqsClient = $sqsClient;
+        $this->params = $params;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $output->writeln('Hello QuocThang!!');
-        $output->writeln('Whoa! You so beautiful honey');
+        $queueUrl = $this->params->get('sqsUrl');
+        try {
+            $result = $this->sqsClient->receiveMessage(array(
+                'AttributeNames' => ['SentTimestamp'],
+                'MaxNumberOfMessages' => 1,
+                'MessageAttributeNames' => ['All'],
+                'QueueUrl' => $queueUrl,
+                'WaitTimeSeconds' => 0,
+            ));
+            if (!empty($result->get('Messages'))) {
+                $output->writeln('Worker has received message: ');
+                $output->writeln($result->get('Messages')[0]['Body']);
+                $result = $this->sqsClient->deleteMessage([
+                    'QueueUrl' => $queueUrl,
+                    'ReceiptHandle' => $result->get('Messages')[0]['ReceiptHandle']
+                ]);
+            } else {
+                $output->writeln("No messages in queue.");
+            }
+        } catch (AwsException $e) {
+            error_log($e->getMessage());
+        }
 
         return Command::SUCCESS;
     }
